9831e9a41062a8e7daac8d04176948e7d52d4818
kwstarikanos <kwstarikanos@gmail.com>
2015-08-22 12:11:51 +0300

    Create page_loader.css
    Create page_loader.css file for page_loader.php.

--- view/building/head.php
+++ view/building/head.php
@@ -7,31 +7,22 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 		<link rel="stylesheet" type="text/css" href="view/css/style.css">
 		<link rel="stylesheet" type="text/css" href="view/css/alerts.css">
-		<?php
-		//if($isMobile == true)
-		//{
-			?>
-			<!-- <meta http-equiv="refresh" content="300"> -->
-			<link rel="stylesheet" type="text/css" href="view/css/debug.css">
-			
-			<link rel="stylesheet" type="text/css" href="view/css/header.css">
-			
-			<link rel="stylesheet" type="text/css" href="view/css/access.css">
-			<link rel="stylesheet" type="text/css" href="view/css/searchform.css">
-			<link rel="stylesheet" type="text/css" href="view/css/toolbar.css">
-			<link rel="stylesheet" type="text/css" href="view/css/loader.css">
-			<link rel="stylesheet" type="text/css" href="view/css/map.css">
-			<link rel="stylesheet" type="text/css" href="view/css/jqpagination.css">
-			<link rel="stylesheet" type="text/css" href="view/css/footer.css">
-			<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script> -->
-			<!-- <script src="view/js/jquery.min.js"></script> -->
-			<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-			<script type="text/javascript" src="view/js/jquery.jqpagination.min.js"></script>
-			<script type="text/javascript" src="view/js/jquery.nicescroll.min.js"></script>
-			<script type="text/javascript" src="view/js/code.js"></script>
-			
-		<?php 
-		//}
-		?>
+		<!-- <meta http-equiv="refresh" content="300"> -->
+		<link rel="stylesheet" type="text/css" href="view/css/debug.css">
+		<link rel="stylesheet" type="text/css" href="view/css/header.css">
+		<link rel="stylesheet" type="text/css" href="view/css/access.css">
+		<link rel="stylesheet" type="text/css" href="view/css/searchform.css">
+		<link rel="stylesheet" type="text/css" href="view/css/toolbar.css">
+		<link rel="stylesheet" type="text/css" href="view/css/loader.css">
+		<link rel="stylesheet" type="text/css" href="view/css/page_loader.css">
+		<link rel="stylesheet" type="text/css" href="view/css/map.css">
+		<link rel="stylesheet" type="text/css" href="view/css/jqpagination.css">
+		<link rel="stylesheet" type="text/css" href="view/css/footer.css">
+		<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script> -->
+		<!-- <script src="view/js/jquery.min.js"></script> -->
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="view/js/jquery.jqpagination.min.js"></script>
+		<script type="text/javascript" src="view/js/jquery.nicescroll.min.js"></script>
+		<script type="text/javascript" src="view/js/code.js"></script>
 	</head>
 	<body>
